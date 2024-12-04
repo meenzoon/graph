cfd0573295b11e38fa247d3e97d6846bb1b85d39
meenzoon <rlaalswns523@gmail.com>
2024-12-04 21:29:30 +0900

    janusgraph vertex 추가시에 index 추가하는 부분 추가

--- janusgraph-manager/src/main/java/kr/co/ncdata/janus/JanusgraphManager.java
+++ janusgraph-manager/src/main/java/kr/co/ncdata/janus/JanusgraphManager.java
@@ -1,56 +1,82 @@
 package kr.co.ncdata.janus;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.janusgraph.core.Cardinality;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 import org.janusgraph.core.JanusGraphTransaction;
+import org.janusgraph.core.PropertyKey;
+import org.janusgraph.core.schema.JanusGraphManagement;
+import org.janusgraph.graphdb.database.management.ManagementSystem;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+@Slf4j
 public class JanusgraphManager {
 	JanusGraph graph;
-	JanusGraphTransaction jgt;
 
 	public JanusgraphManager() {
-		graph = JanusGraphFactory.open("conf/janusgraph-hbase.properties");
-		jgt = graph.newTransaction();
+		graph = JanusGraphFactory.open("/home/janus/janusgraph-1.0.0/conf/janusgraph-hbase-es.properties");
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		JanusgraphManager jm = new JanusgraphManager();
-		jm.proc();
+		try {
+			jm.proc();
+		} catch(InterruptedException e) {
+			log.error("", e);
+		}
 	}
 
-	public void proc() {
+	public void proc() throws InterruptedException {
 		int count = 0;
 		int index = 0;
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(""))) {
+		log.info("add index start");
+
+		JanusGraphManagement mgmt = graph.openManagement();
+
+		// 속성 키 정의
+		PropertyKey name = mgmt.makePropertyKey("nodeId").dataType(String.class).cardinality(Cardinality.SINGLE).make();
+
+		// 인덱스 생성
+		mgmt.buildIndex("NODE_ID", Vertex.class).addKey(name).buildCompositeIndex();
+
+		mgmt.commit();
+		ManagementSystem.awaitGraphIndexStatus(graph, "NODE_ID").call();
+
+		log.info("add index end");
+
+		JanusGraphTransaction jgt = graph.newTransaction();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader("/home/janus/map/daegu_node.csv"))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				String[] col = line.split(",");
 
-				jgt.addVertex(T.label, "osmNode", "id", col[0], "latitude", col[1], "longitude", col[2]);
+				jgt.addVertex(T.label, "node", "nodeId", col[0], "latitude", col[1], "longitude", col[2]);
 
 				++count;
 				if (++index % 10000 == 0) {
 					jgt.commit();
 					jgt = graph.newTransaction();
-					System.out.println("count:" + count);
+					log.info("count: {}", count);
 					index = 0;
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error("", e);
 		}
 
 		if (jgt != null && jgt.isOpen()) {
 			jgt.commit();
 			jgt.close();
-			System.out.println("count:" + count);
+			log.info("end jgt count: {}", count);
 		}
-		if (graph != null & graph.isOpen()) {
+		if (graph != null && graph.isOpen()) {
 			graph.close();
 		}
 	}
