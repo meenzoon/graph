a454460f22de0faadd1c11016c71a960c732b261
meenzoon <rlaalswns523@gmail.com>
2024-12-04 21:33:55 +0900

    edge 추가 부분 logging framework 적용

--- janusgraph-manager/src/main/java/kr/co/ncdata/janus/JanusGraphEdge.java
+++ janusgraph-manager/src/main/java/kr/co/ncdata/janus/JanusGraphEdge.java
@@ -1,5 +1,7 @@
 package kr.co.ncdata.janus;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
@@ -11,14 +13,18 @@ import org.janusgraph.core.JanusGraphVertex;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+@Slf4j
 public class JanusGraphEdge {
 	JanusGraph graph;
 	JanusGraphTransaction jgt;
+	GraphTraversalSource g;
 
 	public JanusGraphEdge() {
 		System.out.println("graph open");
-		graph = JanusGraphFactory.open("janusgraph-hbase.properties");
+		graph = JanusGraphFactory.open("/home/janus/janusgraph-1.0.0/conf/janusgraph-hbase-es.properties");
+		//graph = JanusGraphFactory.open("C:\\tools\\map\\janusgraph-hbase-es.properties");
 		System.out.println("graph new transaction");
+		g = graph.traversal();
 		jgt = graph.newTransaction();
 	}
 
@@ -28,47 +34,60 @@ public class JanusGraphEdge {
 	}
 
 	public void proc(){
-		int count = 0;
 		int index = 0;
 
-		System.out.println("read csv");
-		// /home/janus/map/daegu_way.csv
-		try (BufferedReader reader = new BufferedReader(new FileReader(""))) {
+		log.info("read csv");
+		try (BufferedReader reader = new BufferedReader(new FileReader("/home/janus/map/daegu_way.csv"))) {
+		//try (BufferedReader reader = new BufferedReader(new FileReader("C:\\tools\\map\\daegu_way.csv"))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
-				if(++count == 1)
+				if(++index == 1)
 					continue;
 
+				if(index % 100 == 0){
+					log.info("index: {}", index);
+				}
 				String[] col = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)",-1);
 				String[] nodes = col[1].split(",");
 
 				String startNode = nodes[0].replace("\"", "").replace("[", "").trim();
 				String endNode = nodes[nodes.length - 1].replace("\"", "").replace("]", "").trim();
-				System.out.println("id: " + col[0] + ", startNode: " + startNode + ", endNode: " + endNode);
+
 
 				//Vertex startVertex = .next();
 				//System.out.println("find complete startVetext");
 				//Vertex endVertex = .next();
 
-				//Vertex startVertex = jgt.getVertex(startNode);
-				//Vertex endVertex = jgt.getVertex(endNode);
 				//System.out.println("add Edge");
-				jgt.traversal().addE("way")
-					.from(__.V().has("id", startNode))
-					.to(__.V().has("id", endNode));
+				Vertex startVertex;
+				Vertex endVertex;
+
+				try {
+					startVertex = g.V().has("nodeId", startNode).next();
+					endVertex = g.V().has("nodeId", endNode).next();
+
+					g.V(startVertex).addE("way").to(endVertex).next();
+				} catch(Exception e) {
+					log.error("index: {}, nodeId: {}, startNode: {}, endNode: {}", index, col[0], startNode, endNode);
+					continue;
+				}
+
 				//startVertex.addEdge("way", endVertex);
+
+				/*
+				jgt.traversal().addE("way")
+					.from(  __.V().has("id", startNode))
+					.to(__.V().has("id", endNode));
+				 */
 
 				//jgt.addVertex(T.label, "osmNode", "id", col[0], "latitude", col[1], "longitude", col[2]);
 
-				if (++index % 10000 == 0) {
-					jgt.commit();
-					jgt = graph.newTransaction();
-					System.out.println("count:" + count);
-					index = 0;
-				}
+				jgt.commit();
+				jgt = graph.newTransaction();
+				g.tx().commit();
 			}
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			log.error("", e);
 		} finally {
 			if(jgt != null && jgt.isOpen()){
 				jgt.commit();
@@ -77,6 +96,5 @@ public class JanusGraphEdge {
 			if(graph != null && graph.isOpen())
 				graph.close();
 		}
-
 	}
 }
