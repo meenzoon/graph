eedebcd7f51cfb7e83b863a3566d5042c809e2f4
meenzoon <rlaalswns523@gmail.com>
2024-12-22 23:41:37 +0900

    config 파일 추가

--- janusgraph-manager/src/main/java/kr/co/ncdata/janus/JanusManager.java
+++ janusgraph-manager/src/main/java/kr/co/ncdata/janus/JanusManager.java
@@ -3,6 +3,32 @@ package kr.co.ncdata.janus;
 import java.io.File;
 
 public abstract class JanusManager {
-	private static final String CONF_PATH = System.getProperty("os.name").toLowerCase().contains("win") ? "C:\\tools\\map" : "/home/janus/janusgraph-1.0.0/conf";
+	private static final String OS_ENV = System.getProperty("os.name").toLowerCase();
+	private static final boolean ENV_CONFIG = OS_ENV.contains("win");
+	/**
+	 * janusgraph config path
+	 */
+	private static final String CONF_PATH = ENV_CONFIG ? "C:\\tools\\map" : "/home/janus/janusgraph-1.0.0/conf";
+	/**
+	 * janusgraph properties file name
+	 */
 	public static final String PROP_FILE_NAME = CONF_PATH + File.separator + "janusgraph-hbase-es.properties";
+
+	/**
+	 * 데이터 저장 위치
+	 */
+	private static final String DATA_PATH = ENV_CONFIG ? "C:\\tools\\map" : "/home/janus/map";
+
+	/**
+	 * OSM Node, Way File
+	 */
+	public static final String OSM_NODE_FILE = DATA_PATH + File.separator + "daegu_node.csv";
+	public static final String OSM_WAY_FILE = DATA_PATH + File.separator + "daegu_way.csv";
+
+	/**
+	 * 국가표준노드링크 File
+	 */
+	private static final String ITS_FILE_PATH = DATA_PATH + File.separator + "nodelink";
+	public static final String ITS_NODE_FILE = ITS_FILE_PATH + File.separator + "MOCT_NODE.shp";
+	public static final String ITS_LINK_FILE = ITS_FILE_PATH + File.separator + "MOCT_LINK.shp";
 }
